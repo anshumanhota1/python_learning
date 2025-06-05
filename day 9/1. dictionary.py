7fb606254f50ab789a84330274d90d84fcab3bae
Anshuman Hota <anshumanhota1@gmail.com>
2025-06-05 17:36:02 +0530

    adding day 9

---
+++ day 9/1. dictionary.py
@@ -0,0 +1,3 @@
+# A dictionary in Python functions similarly to a dictionary in real life. 
+# It's a data structure that allows us to associate a key to a value and pair the two pieces of data together.
+
